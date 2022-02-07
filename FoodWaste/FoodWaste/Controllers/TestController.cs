b6cc5daacd68dd0f0bac70a654632a41833e9e9a
Patrick Jahn <devpatrickjahn@gmail.com>
2022-02-07 16:14:27 +0100

    Salling intergration api

---
+++ FoodWaste/FoodWaste/Controllers/TestController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+
+namespace FoodWaste.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class TestController : ControllerBase
+    {
+   
+        private readonly ILogger<TestController> _logger;
+
+        public TestController(ILogger<TestController> logger)
+        {
+            _logger = logger;
+        }
+
+       
+
+        [HttpGet]
+        public String GetTest()
+        {
+            var salling = new FoodWasteHTTPCall.Salling();
+            var e = salling.GetProductAsync("https://api.sallinggroup.com/v2/stores/");
+      
+            // Serialize
+            return "Welcome to foodwaste API " + e;
+        }
+      
+    }
+}
---
+++ FoodWaste/FoodWaste/Salling.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Net;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+
+namespace FoodWasteHTTPCall
+{
+    public class Salling
+    {
+         private string Token = "Bearer cf09e708-cba9-4d57-9f0b-ff1102610685";
+        private readonly HttpClient HttpClient;
+        public Salling()
+        {
+        HttpClient = new HttpClient();
+	
+        }
+
+
+        public  async Task<string> GetProductAsync(string path)
+        {
+            try{
+            using (var request = new HttpRequestMessage(HttpMethod.Get, path))
+            {
+                request.Headers.Authorization = new AuthenticationHeaderValue("Bearer", "cf09e708-cba9-4d57-9f0b-ff1102610685");
+                var response = await HttpClient.SendAsync(request);
+                var e =  await response.Content.ReadAsStringAsync();
+                return e.ToString();
+            }
+            }catch(Exception ex){
+                Console.WriteLine(ex);
+                return "Error";
+            }
+        }
+    }
+}
