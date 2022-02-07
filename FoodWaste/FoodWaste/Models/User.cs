12f562864b524206ee0250ea9af7f8a31302c5fa
1337k1ng <madsjohanbech@live.dk>
2022-02-07 13:57:02 +0100

    user klasse

---
+++ FoodWaste/FoodWaste/Models/User.cs
@@ -0,0 +1,20 @@
+
+namespace FoodWaste {
+
+    public class User {
+
+    
+int id; 
+string userName; 
+string userPassword; 
+string email;
+
+        public User(string userName, string userPassword, string email)
+        {
+            this.userName = userName;
+            this.userPassword = userPassword;
+            this.email = email;
+            
+        }
+    }
+}
