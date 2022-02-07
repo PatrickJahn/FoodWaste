a65f4521cb420d8954bb0879396a5a82d8051fb5
Patrick Jahn <devpatrickjahn@gmail.com>
2022-02-07 13:58:41 +0100

    Clean up

---
+++ FoodWaste/FoodWaste/Controllers/TestController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace FoodWaste.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class TestController : ControllerBase
+    {
+   
+        private readonly ILogger<TestController> _logger;
+
+        public TestController(ILogger<TestController> logger)
+        {
+            _logger = logger;
+        }
+
+       
+
+        [HttpGet]
+        public String GetTest()
+        {
+            return "Welcome to foodwaste API";
+        }
+      
+    }
+}
