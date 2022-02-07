fd01f4db23e4a07234c4c03c2983ab0eca8d70b3
1337k1ng <madsjohanbech@live.dk>
2022-02-07 14:34:57 +0100

    gettter&setters

--- FoodWaste/FoodWaste/Models/User.cs
+++ FoodWaste/FoodWaste/Models/User.cs
@@ -14,7 +14,35 @@ string email;
             this.userName = userName;
             this.userPassword = userPassword;
             this.email = email;
-            
+
         }
+
+     public int getId() {
+         return this.id;
+     }
+    
+     public int setId() {
+         return this.id;
+     }
+
+
+    public string getUserName() {
+        return this.userName;
+    }
+
+    public string setUserName() {
+        return this.userName;
+    }
+    
+
+    public string getEmail() {
+        return this.email;
+    }
+
+    public string setEmail() {
+        return this.email;
+    }
+
+
     }
 }
